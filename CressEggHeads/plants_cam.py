a6a51bd83ac33cf57df6a380b692e9a6a2c14705
alanliburd <allanliburd@zohomail.com>
2020-10-01 01:22:56 -0400

    Sleep after setting the AWB/exposure/img effect

--- CressEggHeads/plants_cam.py
+++ CressEggHeads/plants_cam.py
@@ -13,7 +13,7 @@ if test:
     # pcam.stop_preview()
 
 # EXPOSURE: off, auto, night, nightpreview, backlight, spotlight, sports, snow, beach, verylong, fixedfps, antishake, fireworks
-# AWB:      off, auto, sunlight, cloudy, shade, tungsten, flouorescent, incancedescent, flash, horizon
+# AWB:      off, auto, sunlight, cloudy, shade, tungsten, fluorescent, incandescent, flash, horizon
 # EFFECT:   none, negative, solarize, sketch, denoise, emboss, oilpaint, hatch, gpen, pastel, watercolor, film, blur,
 #       :   saturation, colorswap, washedout, posterize, colorpaint, colorbalance, cartoon, deinterlace1-2
 
@@ -30,19 +30,19 @@ if track:
             timestamp = "{0:%Y}-{0:%m}-{0:%d}_{0:%H}H{0:%M}M".format(currentTime)
             image_name = "plants" + timestamp
             pcam.start_preview()
-            sleep(11)
             if currentTime.hour > 17 or currentTime.hour < 7:
                 pcam.exposure_mode = 'verylong'
                 # pcam.awb_mode = 'auto'
                 # pcam.image_effect = 'none'
             else:
-                # pcam.exposure_mode = 'verylong'
-                pcam.awb_mode = 'sunlight'
-                # pcam.image_effect = 'none'
-            #pcam.annotate_background = Color('white')
+                pcam.exposure_mode = 'auto'
+                pcam.awb_mode = 'auto'
+                # pcam.image_effect = 'negative'
+            # pcam.annotate_background = Color('white')
             pcam.annotate_foreground = Color('black')
             pcam.annotate_text_size = 32
             pcam.annotate_text = "Timestamp: %s" % timestamp
+            sleep(11)
             pcam.capture(pdirectory + image_name + '.jpg')
             pcam.stop_preview()
             pcam.close() # allow cam to be used by a different script
