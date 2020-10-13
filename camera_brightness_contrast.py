1dfad232fa629f3c1c3ad8b4844a0127104ff592
alanliburd <allanliburd@zohomail.com>
2020-10-12 20:39:58 -0400

    Added a saturation loop; \n Update code to catch keyboard interrupt messages during the range loops; \n Added a function to print the camera message

--- camera_brightness_contrast.py
+++ camera_brightness_contrast.py
@@ -1,7 +1,7 @@
 from time import sleep
 from picamera import PiCamera, Color
 import logging
-camera = PiCamera(resolution=(720, 540), framerate=30)
+camera = PiCamera(resolution=(720, 540))
 # Wait for the automatic gain control to settle
 sleep(2)
 
@@ -9,27 +9,65 @@ logger = logging.getLogger (__name__)
 logger.setLevel(level=logging.DEBUG)
 logging.basicConfig(level=logging.DEBUG)
 
+def CamMessage():
+    global camera
+    camera.annotate_text =  "Contrast: " + str(camera.contrast)
+    camera.annotate_text += "\nBrightness: " + str(camera.brightness)
+    camera.annotate_text += "\nSaturation: " + str(camera.saturation)
+
+
+logger.info("Start processing")
+logger.debug("Preview camera before changes")
+camera.annotate_background = Color('white')
+camera.annotate_foreground = Color('black')
+camera.annotate_text_size = 29
+CamMessage()
+camera.annotate_text += "\nFrameRate Range (low: " + str(camera.framerate_range.low) + ", high: " + str(camera.framerate_range.high) + ")"
 camera.start_preview()
-#camera.annotate_text = "Brightness: " + camera.brightness + "\nContrast: " + camera.contrast
-sleep(4)
+
+try:
+    sleep(4)
+except KeyboardInterrupt:
+    logger.warning ('\n\tUser interrupted preview')
 camera.stop_preview()
 
 sleep(2)
 camera.start_preview()
 logger.debug("Go through the contrast levels")
-for i in range(100):
-    camera.annotate_text = "Contrast: %s" % i
-    #camera.brightness = i
-    camera.contrast = i
-    sleep(0.1)
+try:
+    for i in range(-100, 100, 2):
+        #camera.brightness = i
+        camera.contrast = i
+        CamMessage()
+        sleep(0.3)
+except KeyboardInterrupt:
+    logger.warning ('\n\tUser interrupted contrast loop')
 camera.stop_preview()
+
 sleep(2)
 camera.start_preview()
 logger.debug("Go through the brightness levels")
-for i in range(100):
-    camera.annotate_text = "Brightness: %s" % i
-    camera.brightness = i
-    #camera.contrast = i
-    sleep(0.1)
+camera.contrast = 0 # reset to default contrast
+try:
+    for i in range(100):
+        camera.brightness = i
+        CamMessage()
+        sleep(0.2)
+except KeyboardInterrupt:
+    logger.warning ('\n\tUser interrupted brightness loop')
 camera.stop_preview()
-logger.debug("Done processing")
+
+sleep(2)
+camera.start_preview()
+logger.debug("Go through the saturation levels")
+camera.brightness = 50 # reset to default brightness
+try:
+    for i in range(-100, 100, 2):
+        camera.saturation = i
+        CamMessage()
+        sleep(0.2)
+except KeyboardInterrupt:
+    logger.warning ('\n\tUser interrupted brightness loop')
+camera.stop_preview()
+
+logger.info("Done processing")
